4980e1416afadfa89ec802c5a1aa93b14695c293
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-06 18:48:08 +0700

    mendefinisikan kerangka navigasi dan enum untuk rute

--- app/src/main/java/com/example/navigasikedua/Navigasi.kt
+++ app/src/main/java/com/example/navigasikedua/Navigasi.kt
@@ -1,2 +1,26 @@
 package com.example.navigasikedua
 
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasikedua.view.HalamanFormulir
+import com.example.navigasikedua.view.HalamanList
+import com.example.navigasikedua.view.HalamanSelamatDatang
+
+
+enum class PengelolaHalaman {
+    Home,
+    List,
+    Form,
+}
+
+@Composable
+fun NavigasiAplikasi(
+    navController: NavHostController = rememberNavController()
+) 
